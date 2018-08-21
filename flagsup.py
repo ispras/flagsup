30e5563185ae1344bda490428d68d9f8fcf82bb5
Vladislav Ivanishin <vlad@ispras.ru>
2018-08-21 19:34:52 +0300

    flagsup: output format: canonical flags, diffs, comp_dirs (#CUs)

--- flagsup.py
+++ flagsup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
+from itertools import count
 
 from elftools.elf.elffile import ELFFile
 
@@ -22,16 +23,33 @@ def process_file(filename):
                     comp_dir = die.attributes['DW_AT_comp_dir'].value
                     producer = die.attributes['DW_AT_producer'].value
                     if producer not in flag_sets:
-                        flag_sets[producer] = set()
-                    flag_sets[producer].add(comp_dir)
+                        flag_sets[producer] = {}
+                    if comp_dir not in flag_sets[producer]:
+                        flag_sets[producer][comp_dir] = set()
+                    flag_sets[producer][comp_dir].add(die.get_full_path())
                 except KeyError:
                     pass
 
-flag_sets = {} # producer -> set(comp_dirs)
+flag_sets = {} # producer -> (comp_dir -> set(full_paths))
 
 process_file(sys.argv[1])
 
-for producer in flag_sets:
-    print(producer)
+n_cus = lambda x: sum(len(paths) for paths in flag_sets[x])
+producers = sorted(flag_sets, key=n_cus, reverse=True)
+
+canonical = producers[0]
+print('Canonical compile string (%d compile units): \n%s' %
+      (n_cus(canonical), canonical))
+print('compile dirs (units):')
+for comp_dir in flag_sets[canonical]:
+    print("\t%s (%d)" % (comp_dir, len(flag_sets[canonical][comp_dir])))
+print()
+
+for producer,i in zip(producers[1:], count(start=2)):
+    print("Diff for flag set %d (%d compile units):" % (i, n_cus(producer)))
+    print("+++:", b" ".join(sorted(set(producer.split()) - set(canonical.split()))))
+    print("---:", b" ".join(sorted(set(canonical.split()) - set(producer.split()))))
+    print('compile dirs (units):')
     for comp_dir in flag_sets[producer]:
-        print ("\t%s" % comp_dir)
+        print("\t%s (%d)" % (comp_dir, len(flag_sets[producer][comp_dir])))
+    print()
